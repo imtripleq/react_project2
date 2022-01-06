a4141d8380392226c2af7ca180034decf0106b72
imtripleq <flamework89@hotmail.com>
2022-01-06 16:19:28 +1100

    updated README

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,20 @@
 import { Box, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import React from "react";
 
 const Welcome = () => {
+  const useStyles = makeStyles({
+    page: {},
+    title: { margin: "30px 0px" },
+  });
+
+  const classes = useStyles();
+
   return (
-    <Box>
-      <Typography variant="h2">Welcome!</Typography>
+    <Box className={classes.page}>
+      <Box className={classes.title}>
+        <Typography variant="h2">Welcome!</Typography>
+      </Box>
     </Box>
   );
 };
